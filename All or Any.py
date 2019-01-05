bc84ee41322cbe75ffa942fa440a77ee0870c215
niniDev <31903811+niniDev@users.noreply.github.com>
2019-01-04 23:59:55 -0300

    Update All or Any.py

--- All or Any.py
+++ All or Any.py
@@ -1,4 +1,5 @@
-'''
+''' 
+CREDITS TO HACKERRANK FOR THIS CHALLENGE
 TASK
 You are given a space separated list of integers. If all the integers are positive, then you need to check if any integer is a palindromic integer.
 
